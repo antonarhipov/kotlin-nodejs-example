058dba7df4109683e0e19e512263e120f85a0fd5
Anton Arhipov <ant.arhipov@gmail.com>
2020-04-15 11:24:09 +0300

    typed version

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -1,21 +1,34 @@
-external fun require(module:String):dynamic
+//region http type mapping for node.js
+@JsModule("http")
+@JsNonModule
+external object http {
+    fun createServer(
+            options: dynamic = definedExternally,
+            requestListener: (request: IncomingMessage, response: ServerResponse) -> Unit
+    ): Server
+    class Server {
+        fun listen(port: Int, hostname: String, callback: () -> Unit)
+    }
+    interface IncomingMessage
+    interface ServerResponse {
+        var statusCode: Int
+        fun setHeader(key: String, value: String)
+        fun end(s: String)
+    }
+}
+//endregion
 
 fun main() {
     println("Hello JavaScript!")
-
-    val http = require("http")
     val hostname = "127.0.0.1"
     val port = 3000
-
     println("Server will try to run at http://${hostname}:${port}/")
-
-    val server = http.createServer{req, res ->
+    val server = http.createServer { req, res ->
         res.statusCode = 200
         res.setHeader("Content-Type", "text/plain")
         res.end("Hello World!!!!\n")
     }
-
-    server.listen(port, hostname, fun() {
+    server.listen(port, hostname) {
         println("Server running at http://${hostname}:${port}/")
-    })
+    }
 }
